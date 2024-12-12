c0e637b3b3ab6840e8dd8750db7be197ea56359d
Eric Pelz <github@ericpelz.com>
2024-12-11 22:04:25 -0800

    2024-12, part 2

--- 2024/challenges/12/index.ts
+++ 2024/challenges/12/index.ts
@@ -20,29 +20,41 @@ interface Region {
     plots: Coordinate[];
 }
 
-const containsCoordinate = (region: Region, [x,y]: Coordinate) => region.plots.some(([x1,y1]) => x1 === x && y1 === y);
-const isNeighbor = (region: Region, [x,y]: Coordinate) => {
-    return (["N","E","S","W"] as Direction[]).some(dir => containsCoordinate(region, moveCoordinate([x,y], dir)))
-}
+const hasCoord = (region: Region, [x, y]: Coordinate) =>
+    region.plots.some(([x1, y1]) => x1 === x && y1 === y);
+const isNeighbor = (region: Region, [x, y]: Coordinate) => {
+    return (["N", "E", "S", "W"] as Direction[]).some((dir) =>
+        hasCoord(region, moveCoordinate([x, y], dir)),
+    );
+};
 const Schema = MatrixSchema(CharSchema, "").transform((input) => {
     let regions: Region[] = [];
 
     input.forEach((row, rowIdx) => {
         row.forEach((col, colIdx) => {
-            const adjoiningRegions = regions.filter((region) => region.type === col && isNeighbor(region, [rowIdx, colIdx]));
-            // assert(adjoiningRegions.length <= 1, "Adjoining regions must be unique");
+            const adjoiningRegions = regions.filter(
+                (region) =>
+                    region.type === col && isNeighbor(region, [rowIdx, colIdx]),
+            );
+
             if (adjoiningRegions.length === 1) {
                 adjoiningRegions[0].plots.push([rowIdx, colIdx]);
             } else if (adjoiningRegions.length === 0) {
                 regions.push({ type: col, plots: [[rowIdx, colIdx]] });
             } else {
                 // Non-unique adjoining regions
-                regions.push({ type: col, plots: [...adjoiningRegions.flatMap(r => r.plots), [rowIdx, colIdx]] });
-                regions = regions.filter(r => !adjoiningRegions.includes(r));
+                regions.push({
+                    type: col,
+                    plots: [
+                        ...adjoiningRegions.flatMap((r) => r.plots),
+                        [rowIdx, colIdx],
+                    ],
+                });
+                regions = regions.filter((r) => !adjoiningRegions.includes(r));
             }
         });
     });
-    
+
     return { grid: input, regions };
 });
 
@@ -57,14 +69,68 @@ const prettyPrint = (map: Grid): void => {
 
 const regionArea = (region: Region): number => region.plots.length;
 const regionPerimeter = (region: Region): number => {
-    return sum(region.plots.map(([x,y]) => {
-        return (["N","E","S","W"] as Direction[]).filter(dir => !containsCoordinate(region, moveCoordinate([x,y], dir))).length
-    }));
-}
-
-const part1 = ({ grid, regions }: Schema): number => {
-    return regions.map(region => regionArea(region) * regionPerimeter(region)).reduce((acc, val) => acc + val, 0);
+    return sum(
+        region.plots.map(([x, y]) => {
+            return (["N", "E", "S", "W"] as Direction[]).filter(
+                (dir) => !hasCoord(region, moveCoordinate([x, y], dir)),
+            ).length;
+        }),
+    );
 };
 
+const part1 = ({ grid, regions }: Schema): number => {
+    return regions
+        .map((region) => regionArea(region) * regionPerimeter(region))
+        .reduce((acc, val) => acc + val, 0);
+};
 
-main(module, parse, part1);
+const regionSides = (region: Region): number => {
+    return sum(
+        region.plots.map(([x, y]) => {
+            let total = 0;
+            const [N, E, S, W, NE, NW, SE, SW] = (
+                ["N", "E", "S", "W", "NE", "NW", "SE", "SW"] as Direction[]
+            ).map((dir) => moveCoordinate([x, y], dir));
+
+            if (!hasCoord(region, W) && !hasCoord(region, N)) total++;
+            if (!hasCoord(region, N) && !hasCoord(region, E)) total++;
+            if (!hasCoord(region, E) && !hasCoord(region, S)) total++;
+            if (!hasCoord(region, S) && !hasCoord(region, W)) total++;
+
+            if (
+                hasCoord(region, W) &&
+                hasCoord(region, S) &&
+                !hasCoord(region, SW)
+            )
+                total++;
+            if (
+                hasCoord(region, W) &&
+                hasCoord(region, N) &&
+                !hasCoord(region, NW)
+            )
+                total++;
+            if (
+                hasCoord(region, E) &&
+                hasCoord(region, S) &&
+                !hasCoord(region, SE)
+            )
+                total++;
+            if (
+                hasCoord(region, E) &&
+                hasCoord(region, N) &&
+                !hasCoord(region, NE)
+            )
+                total++;
+
+            return total;
+        }),
+    );
+};
+
+const part2 = ({ grid, regions }: Schema): number => {
+    return regions
+        .map((region) => regionArea(region) * regionSides(region))
+        .reduce((acc, val) => acc + val, 0);
+};
+
+main(module, parse, part1, part2);
